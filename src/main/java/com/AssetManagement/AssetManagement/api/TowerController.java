e184f6fb0f418e10bbbbd60c893842ce895f0339
Thanyani Tshikonwani <thanyanitshikonwani@umuzi.org>
2020-02-01 03:38:22 +0200

    PostgresSQl DataSource Added

--- src/main/java/com/AssetManagement/AssetManagement/api/TowerController.java
+++ src/main/java/com/AssetManagement/AssetManagement/api/TowerController.java
@@ -11,7 +11,7 @@ import java.util.List;
 import java.util.UUID;
 
 
-@RequestMapping("api/v1/tower")
+@RequestMapping("home/tower")
 @RestController
 public class TowerController {
     private final TowerService towerService;
